5a38dd4c74af84dc999cfc0645b5b292f7748467
JT Wolohan <jwolohan@gmail.com>
2016-05-06 12:08:32 -0400

    First chapter of code

--- baby.hs
+++ baby.hs
@@ -9,3 +9,9 @@ doubleSmallNumber x = if x > 100
                          else x*2
 
 doubleSmallPlusOne x = (if x > 100 then x else x*2) + 1
+
+boomBangs xs  = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length'  xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
