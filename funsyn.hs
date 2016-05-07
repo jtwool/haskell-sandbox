41abf694c949edf3c454f07467ca83cfc714ac1f
JT Wolohan <jwolohan@gmail.com>
2016-05-07 14:04:05 -0400

    Further Chap 3 progress

--- funsyn.hs
+++ funsyn.hs
@@ -30,3 +30,31 @@ max' :: (Ord a) => a -> a -> a
 max' a b
  | a > b = a
  | otherwise = b
+
+bmi :: (RealFloat a) => a -> a -> String
+bmi weight height
+    | x <= skinny = "You're underweight!" 
+    | x <= normal = "You're normal weight."
+    | x <= fat    = "You're overweight!"
+    | otherwise = "You're very overweight."
+    where x = (weight * 0.45) / ((height * 0.025)^2)
+ 	  (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+bmiPair :: (RealFloat a) => [(a,a)] -> [a]
+bmiPair xs = [bmi w h | (w, h) <- xs]
+	where bmi weight height = weight / height ^2
+
+initialGetter :: String -> String -> String
+initialGetter firstname lastname = [f] ++ "." ++ [l] ++ "."
+	where (f:_) = firstname
+	      (l:_) = lastname
+
+initialGetter' :: String -> String -> String
+initialGetter' (first:_) (last:_) = [first] ++ "." ++ [last] ++ "."
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+	let  sideArea = 2*pi*r*h
+	     topArea = pi*r^2
+	in sideArea + 2 * topArea
+
